fa7702edb97f8c65d52ac001bcbb6ff475efb469
linhhm24 <linh.hm24@gmail.com>
2020-02-24 22:30:38 +0700

    Add file

---
+++ my1.js
@@ -0,0 +1,4 @@
+let i=0;
+for (i=0; i<5; i=i+1){
+    document.write('The number is '+i+'<br>');
+}
---
+++ my2.js
@@ -0,0 +1,7 @@
+let input_number=parseInt(prompt('Nhap vao 1 so bat ky lon hon 0'));
+let sum=null;
+let i =0;
+for (i=1; i<=input_number; i=i+1) {
+    sum=sum+i;
+}
+alert('Tong cua cac so tren la '+ sum);
